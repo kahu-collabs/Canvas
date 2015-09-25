804a8d1b67a6f1f12d8e0b6f18cbb7afba0ecedd
name": null <email": null>
2015-09-25 12:06:08 +1200

    Change confusing ding variable

--- source/renderer.js
+++ source/renderer.js
@@ -3,7 +3,7 @@ function Renderer() {
 }
 
 Renderer.prototype.makeSound = function(sound) {
-  audio = new Audio(sound);
+  var audio = new Audio(sound);
   audio.play();
 }
 
@@ -27,8 +27,6 @@ Renderer.prototype.move = function(obj, direction, dist){
 Renderer.prototype.error = function(command) {
     console.log("I don't understand: " + command)
 }
-
-var audio = new Audio('assets/ding.mp3');
 
 module.exports = Renderer
 
